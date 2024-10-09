b72907c13104ec9231af50b693292a8b3cd2169a
melak-12 <melakabebeee@gmail.com>
2024-10-09 23:55:23 +0300

    chore: Update employee model to include required fields

--- server/modules/employee/models/employeeModel.ts
+++ server/modules/employee/models/employeeModel.ts
@@ -1,12 +1,132 @@
-import { Schema, Document, model } from 'mongoose';
-import { IEmployee } from '../types/employeeTypes';
+import { Schema,model,Document } from "mongoose";
+import { Education, IEmployee, Roles } from "../types/employeeTypes";
 
-const employeeSchema = new Schema<IEmployee>({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    age: { type: Number, required: true },
-    position: { type: String, required: true },
+// Schema for Education
+export const educationSchema = new Schema<Education>({
+  id: { type: String },
+  fieldofstudy: { type: String },
+  institution: { type: String, required: true },
+  graduationYear: { type: Number, required: true },
+  educationLevel: { type: String, required: true },
 });
 
-// Create and export the employee model
-export default model<IEmployee>('Employee', employeeSchema);
+const employeeSchema = new Schema<IEmployee>(
+  {
+    title: { type: String, required: true },
+    empId: { type: String },
+    firstName: { type: String, required: true },
+    middleName: { type: String },
+    lastName: { type: String, required: true },
+    birthday: { type: Date, required: true },
+    gender: { type: String, required: true },
+    position: { type: String, required: true },
+    department: { type: String, required: true },
+    photo: { type: String },
+    ethnicity: { type: String, required: true },
+    phoneNumber: {
+      prefix: { type: String, required: true },
+      number: { type: String, required: true },
+    },
+    email: { type: String, required: true },
+    currentAddress: {
+      region: { type: String },
+      subcity: { type: String },
+      woreda: { type: String },
+      houseNumber: { type: String },
+      leyuBota: { type: String },
+    },
+    salary: { type: String, required: true },
+    educationLevel: { type: String },
+    education: [educationSchema],
+    birthplaceInfo: {
+      region: { type: String },
+      subcity: { type: String },
+      woreda: { type: String },
+      houseNumber: { type: String },
+      leyuBota: { type: String },
+    },
+    motherInformation: {
+      firstName: { type: String, required: true },
+      middleName: { type: String },
+      lastName: { type: String, required: true },
+      phoneNumber: {
+        prefix: { type: String, required: true },
+        number: { type: String, required: true },
+      },
+    },
+    emergencyContact: {
+      info: {
+        firstName: { type: String, required: true },
+        middleName: { type: String },
+        lastName: { type: String, required: true },
+        relationship: { type: String, required: true },
+        phoneNumber: { type: String, required: true },
+        email: { type: String },
+      },
+      address: {
+        region: { type: String },
+        subcity: { type: String },
+        woreda: { type: String },
+        houseNumber: { type: String },
+        leyuBota: { type: String },
+      },
+    },
+    maritalStatus: { type: String, required: true },
+    spouseInfo: {
+      firstName: { type: String },
+      middleName: { type: String },
+      lastName: { type: String },
+      dob: { type: Date },
+      phoneNumber: { type: String },
+      address: {
+        region: { type: String },
+        subcity: { type: String },
+        woreda: { type: String },
+        houseNumber: { type: String },
+        leyuBota: { type: String },
+      },
+    },
+    divorcedInfo: {
+      divorceDate: { type: Date },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: Object.values(Roles),
+      default: Roles.Employee,
+    },
+    retirementDate: { type: Date },
+    status: { type: String, enum: ['active', 'inactive'], default: 'active' },
+    // New fields added
+    skinColor: { type: String, required: true },
+    noseStructure: { type: String, required: true },
+    eyeColor: { type: String, required: true },
+    hairTexture: { type: String, required: true },
+    height: { type: String, required: true },
+    religion: { type: String, required: true },
+    nationality: { type: String, required: true },
+    employmentDate: { type: Date },
+  },
+  { timestamps: true },
+);
+
+export const Employee = model<IEmployee>('Employee', employeeSchema);
+employeeSchema.pre<IEmployee>('save', async function (next) {
+  if (!this.empId) {
+    const lastEmployee = await Employee.findOne(
+      {},
+      {},
+      { sort: { createdAt: -1 } },
+    );
+    let lastempIdNumber = 0;
+    if (lastEmployee && lastEmployee.empId) {
+      lastempIdNumber = parseInt(lastEmployee.empId.split('-')[1]);
+    }
+    this.empId = `FPC-${(lastempIdNumber + 1).toString().padStart(4, '0')}`;
+  }
+  next();
+});
--- server/modules/employee/types/employeeTypes.ts
+++ server/modules/employee/types/employeeTypes.ts
@@ -1,7 +1,110 @@
-// Types for employee module
-export interface IEmployee extends Document {
+
+// Types for the employee module
+export interface IEmployee {
   firstName: string;
   lastName: string;
   age: number;
   position: string;
+  title: string;
+  empId?: string;
+  middleName?: string;
+  birthday: Date;
+  gender: string;
+  department: string;
+  photo?: string;
+  ethnicity: string;
+  phoneNumber: {
+    prefix: string;
+    number: string;
+  };
+  email: string;
+  currentAddress?: {
+    region?: string;
+    subcity?: string;
+    woreda?: string;
+    houseNumber?: string;
+    leyuBota?: string;
+  };
+  salary: string;
+  educationLevel?: string;
+  education?: Education[];
+  birthplaceInfo?: {
+    region?: string;
+    subcity?: string;
+    woreda?: string;
+    houseNumber?: string;
+    leyuBota?: string;
+  };
+  motherInformation: {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    phoneNumber: {
+      prefix: string;
+      number: string;
+    };
+  };
+  emergencyContact: {
+    info: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+      relationship: string;
+      phoneNumber: string;
+      email?: string;
+    };
+    address?: {
+      region?: string;
+      subcity?: string;
+      woreda?: string;
+      houseNumber?: string;
+      leyuBota?: string;
+    };
+  };
+  maritalStatus: string;
+  spouseInfo?: {
+    firstName?: string;
+    middleName?: string;
+    lastName?: string;
+    dob?: Date;
+    phoneNumber?: string;
+    address?: {
+      region?: string;
+      subcity?: string;
+      woreda?: string;
+      houseNumber?: string;
+      leyuBota?: string;
+    };
+  };
+  divorcedInfo?: {
+    divorceDate?: Date;
+  };
+  password: string;
+  role: Roles;
+  retirementDate?: Date;
+  status: 'active' | 'inactive';
+  skinColor: string; 
+  noseStructure: string; 
+  eyeColor: string; 
+  hairTexture: string; 
+  height: string; 
+  religion: string; 
+  nationality: string; 
+  employmentDate: Date;
+}
+
+// Types for the Education schema
+export interface Education {
+  id?: string;
+  fieldofstudy?: string;
+  institution: string;
+  graduationYear: number;
+  educationLevel: string;
+}
+
+// Enum for roles
+export enum Roles {
+  Employee = 'employee',
+  Manager = 'manager',
+  DepartmentHead = 'department head',
 }
