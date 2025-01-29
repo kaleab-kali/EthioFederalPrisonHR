97ab087b625ceeeacf4206a424a5b794a21304a7
melak-12 <melakabebeee@gmail.com>
2025-01-29 21:11:18 +0300

    add updateCenter functionality and route for center management

--- server/modules/centers/routes/centerRoute.ts
+++ server/modules/centers/routes/centerRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import {    createCenter, getAllCenters, getCenterById, } from '../controllers/centerController';
+import {    createCenter, getAllCenters, getCenterById, updateCenter, } from '../controllers/centerController';
 
 const router = Router();
 
 router.get('/', getAllCenters);
 router.get('/id', getCenterById);
 router.post('/', createCenter);
+router.put('/id', updateCenter);
 
 
 export default router;
