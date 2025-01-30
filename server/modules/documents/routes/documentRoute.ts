cc024330bbb5c924056807959fbe23b388ab1621
Mulualem Mekonnen <mulualemasfaw2321@gmail.com>
2025-01-30 22:39:13 +0300

    role based added

--- server/modules/documents/routes/documentRoute.ts
+++ server/modules/documents/routes/documentRoute.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import { createDocument, deleteDocument, getDocument, getDocuments, updateDocument } from '../controllers/documentController';
 import fileUpload from 'express-fileupload';
-
+import { checkdocumentStaff } from '../../employee/middlewares/checkRoles';
+import  {authenticate}   from '../../employee/middlewares/authunticate';
 
 const router = Router();
 router.use(fileUpload());
+// COMMENT FOR MESEKIR
+//All routes need centerName
 
-router.get('/', getDocuments);
-router.get('/:id', getDocument);
-router.post('', createDocument);
-router.put('/:id', updateDocument);
-router.delete('/:id', deleteDocument);
+router.get('/:centerName', authenticate, checkdocumentStaff, getDocuments);
+router.get('/:id/:centerName', authenticate, checkdocumentStaff, getDocument);
+router.post('/:centerName', authenticate, checkdocumentStaff, createDocument);
+router.put('/:id/:centerName', authenticate, checkdocumentStaff, updateDocument);
+router.delete('/:id/:centerName', checkdocumentStaff, deleteDocument);
 
 export default router;
