32d730eb418b68e857bcc30f8d84f730ea792fa9
melak-12 <melakabebeee@gmail.com>
2025-11-18 00:15:02 +0300

    refactor: update EmployeeDocuments component for improved readability and maintainability, enhance CORS configuration in server

--- client/src/modules/complaint/layout/ComplaintList.tsx
+++ client/src/modules/complaint/layout/ComplaintList.tsx
@@ -10,7 +10,6 @@
 // import { IComplaintList } from '../types/Complaint';
 // import { useAllComplaints } from '../services/queries';
 
-
 // const columnHelper = createColumnHelper<IComplaintList>();
 
 // const columns = [
@@ -77,7 +76,7 @@
 //           className="flex items-center justify-start gap-2 text-blue-600 hover:text-blue-800 font-medium py-2 px-4 rounded-md hover:bg-gray-200 transition duration-200"
 //           onClick={() =>
 //             downloadFile(
-//               `http://localhost:5000${file}`,
+//               `hhttps://ethio-federal-prison-hr-o4rx.vercel.app${file}`,
 //               `attachment-${index + 1}`
 //             )
 //           }
@@ -118,10 +117,9 @@
 //          }
 //        }, [dataQuery.data]);
 
-
 //     const pdfFunction = () => {
 //         alert('pdf')
-    
+
 //       }
 //       const excelFunction = () => {
 //         alert('excel')
@@ -132,7 +130,7 @@
 //       const printFunction = () => {
 //         alert('print')
 //       }
-    
+
 //       const table = useReactTable({
 //         data: data,
 //         columns,
@@ -169,9 +167,9 @@
 //                             header.getContext()
 //                           )}
 //                           {header.column.columnDef.header !== "actions" && <LuArrowDownUp className="ml-1" size={12} />}
-                          
+
 //                         </div>
-                      
+
 //                     </th>
 //                   ))}
 //                 </tr>
@@ -191,7 +189,7 @@
 //                 }
 //             </tbody>
 //           </table>
-    
+
 //         </div>
 //    </>
 //   )
@@ -199,20 +197,27 @@
 
 // export default ComplaintList
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   useReactTable,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
 } from "@tanstack/react-table";
-import { LuArrowDownUp, LuDownload } from 'react-icons/lu';
-import { IComplaintList } from '../types/Complaint';
-import { useAllComplaints } from '../services/queries';
-import { Document, Page, Text, View, StyleSheet, pdf } from '@react-pdf/renderer';
-import * as XLSX from 'xlsx';
-import { saveAs } from 'file-saver';
-import { CSVLink } from 'react-csv';
+import { LuArrowDownUp, LuDownload } from "react-icons/lu";
+import { IComplaintList } from "../types/Complaint";
+import { useAllComplaints } from "../services/queries";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  pdf,
+} from "@react-pdf/renderer";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
+import { CSVLink } from "react-csv";
 
 const columnHelper = createColumnHelper<IComplaintList>();
 
@@ -245,9 +250,9 @@ const columns = [
     header: () => "Status",
     cell: (info) => {
       return info.getValue() === "pending" ? (
-        <span className="text-cyan-500">{info.getValue()}</span>
+        <span className='text-cyan-500'>{info.getValue()}</span>
       ) : (
-        <span className="text-red-600">{info.getValue()}</span>
+        <span className='text-red-600'>{info.getValue()}</span>
       );
     },
   }),
@@ -273,14 +278,14 @@ const columns = [
       };
 
       return attachments && attachments.length > 0 ? (
-        <div className="flex flex-col gap-2  bg-gray-100 rounded-lg shadow-md">
+        <div className='flex flex-col gap-2  bg-gray-100 rounded-lg shadow-md'>
           {attachments.map((file, index) => (
             <button
               key={index}
-              className="flex items-center justify-start gap-2 text-blue-600 hover:text-blue-800 font-medium py-2 px-4 rounded-md hover:bg-gray-200 transition duration-200"
+              className='flex items-center justify-start gap-2 text-blue-600 hover:text-blue-800 font-medium py-2 px-4 rounded-md hover:bg-gray-200 transition duration-200'
               onClick={() =>
                 downloadFile(
-                  `http://localhost:5000${file}`,
+                  `https://ethio-federal-prison-hr-o4rx.vercel.app${file}`,
                   `attachment-${index + 1}`
                 )
               }
@@ -291,7 +296,7 @@ const columns = [
           ))}
         </div>
       ) : (
-        <span className="text-gray-500">No attachments</span>
+        <span className='text-gray-500'>No attachments</span>
       );
     },
   }),
@@ -299,24 +304,24 @@ const columns = [
 
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'column',
-    backgroundColor: '#E4E4E4',
+    flexDirection: "column",
+    backgroundColor: "#E4E4E4",
     padding: 10,
   },
   table: {
     // display: 'table',
-    width: 'auto',
-    borderStyle: 'solid',
+    width: "auto",
+    borderStyle: "solid",
     borderWidth: 1,
     borderRightWidth: 0,
     borderBottomWidth: 0,
   },
   tableRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   tableCol: {
-    width: '12.5%', // Adjust based on the number of columns
-    borderStyle: 'solid',
+    width: "12.5%", // Adjust based on the number of columns
+    borderStyle: "solid",
     borderWidth: 1,
     borderLeftWidth: 0,
     borderTopWidth: 0,
@@ -350,7 +355,10 @@ const ComplaintList = () => {
     }
   }, [dataQuery.data]);
 
-  const handleDownloadPDF = async (pdfDocument: JSX.Element, fileName: string) => {
+  const handleDownloadPDF = async (
+    pdfDocument: JSX.Element,
+    fileName: string
+  ) => {
     const blob = await pdf(pdfDocument).toBlob();
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
@@ -363,7 +371,7 @@ const ComplaintList = () => {
   const pdfFunction = () => {
     const MyDocument = (
       <Document>
-        <Page size="A4" style={styles.page}>
+        <Page size='A4' style={styles.page}>
           <View style={styles.table}>
             <View style={styles.tableRow}>
               <View style={styles.tableCol}>
@@ -428,16 +436,21 @@ const ComplaintList = () => {
       </Document>
     );
 
-    handleDownloadPDF(MyDocument, 'complaints.pdf');
+    handleDownloadPDF(MyDocument, "complaints.pdf");
   };
 
   const excelFunction = () => {
     const worksheet = XLSX.utils.json_to_sheet(data);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Complaints");
-    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-    saveAs(blob, 'complaints.xlsx');
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    const blob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    saveAs(blob, "complaints.xlsx");
   };
 
   const csvData = data.map((row) => ({
@@ -448,14 +461,16 @@ const ComplaintList = () => {
     category: row.category,
     reason: row.reason,
     status: row.status,
-    attachments: row.attachments ? row.attachments.join(", ") : "No attachments",
+    attachments: row.attachments
+      ? row.attachments.join(", ")
+      : "No attachments",
   }));
 
   const printFunction = () => {
-    const printableTable = document.getElementById('printable-table');
+    const printableTable = document.getElementById("printable-table");
 
     if (!printableTable) {
-      console.error('Printable table not found!');
+      console.error("Printable table not found!");
       return;
     }
 
@@ -476,25 +491,50 @@ const ComplaintList = () => {
 
   return (
     <>
-      <h2 className="font-medium text-gray-600 font-roboto px-0 py-3">
+      <h2 className='font-medium text-gray-600 font-roboto px-0 py-3'>
         List of Complaint Received
       </h2>
-      <div className="flex justify-end gap-2 mb-3">
-        <button className="bg-green-300 rounded-md px-4 text-center text-md" onClick={pdfFunction}>PDF</button>
-        <button className="bg-green-300 rounded-md px-4 text-center text-md" onClick={excelFunction}>EXCEL</button>
-        <CSVLink data={csvData} filename={"complaints.csv"} className="bg-green-300 rounded-md px-4 text-center text-md no-underline">
+      <div className='flex justify-end gap-2 mb-3'>
+        <button
+          className='bg-green-300 rounded-md px-4 text-center text-md'
+          onClick={pdfFunction}
+        >
+          PDF
+        </button>
+        <button
+          className='bg-green-300 rounded-md px-4 text-center text-md'
+          onClick={excelFunction}
+        >
+          EXCEL
+        </button>
+        <CSVLink
+          data={csvData}
+          filename={"complaints.csv"}
+          className='bg-green-300 rounded-md px-4 text-center text-md no-underline'
+        >
           CSV
         </CSVLink>
-        <button className="bg-green-300 rounded-md px-4 text-center text-md" onClick={printFunction}>PRINT</button>
+        <button
+          className='bg-green-300 rounded-md px-4 text-center text-md'
+          onClick={printFunction}
+        >
+          PRINT
+        </button>
       </div>
 
-      <div id="printable-table" className="overflow-x-auto bg-white shadow-md rounded-xl">
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
+      <div
+        id='printable-table'
+        className='overflow-x-auto bg-white shadow-md rounded-xl'
+      >
+        <table className='min-w-full divide-y divide-gray-200'>
+          <thead className='bg-gray-50'>
             {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id} className="">
+              <tr key={headerGroup.id} className=''>
                 {headerGroup.headers.map((header) => (
-                  <th key={header.id} className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th
+                    key={header.id}
+                    className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                  >
                     <div
                       {...{
                         className: header.column.getCanSort()
@@ -507,18 +547,23 @@ const ComplaintList = () => {
                         header.column.columnDef.header,
                         header.getContext()
                       )}
-                      {header.column.columnDef.header !== "actions" && <LuArrowDownUp className="ml-1" size={12} />}
+                      {header.column.columnDef.header !== "actions" && (
+                        <LuArrowDownUp className='ml-1' size={12} />
+                      )}
                     </div>
                   </th>
                 ))}
               </tr>
             ))}
           </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
+          <tbody className='bg-white divide-y divide-gray-200'>
             {table.getRowModel().rows.map((row) => (
-              <tr key={row.id} className="hover:bg-gray-50">
+              <tr key={row.id} className='hover:bg-gray-50'>
                 {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id} className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  <td
+                    key={cell.id}
+                    className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'
+                  >
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </td>
                 ))}
